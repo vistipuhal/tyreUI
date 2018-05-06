ddce97582e8ae981d98bba8e3c30918b6ffcdac8
Kumar <vishal.kumar1@cgi.com>
2018-05-06 09:43:49 +0530

    edit1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -42,7 +42,7 @@ const appRoutes: Routes = [
     TyreDetailsComponent,
     BusDetailsComponent,
     TyreSearchComponent,
-    BusSearchComponent
+    BusSearchComponent,
   ],
   imports: [
     BrowserModule,
